59ee117377bae3c47eaa017c5e212c9330be8295
KUDUPURI TAMMESWAR <147488906+Tammeswar@users.noreply.github.com>
2026-07-28 15:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (51.19%) - LeetSync

--- 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -6,11 +6,7 @@ public:
        
         sort(arr.begin(), arr.end());
          int diff = arr[1] - arr[0];
-        // for(int i=0;i<n;i++){
-        //     if(arr[i] < 0){
-        //         arr[i] = -arr[i];
-        //     }
-        // }
+       
         for(int i=1;i<n-1;i++){
             if(arr[i+1] - arr[i] != diff){
                 return false;
