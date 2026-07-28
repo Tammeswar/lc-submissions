34c8ee0a1fb41edcf98297468009f39be3ca5d55
KUDUPURI TAMMESWAR <147488906+Tammeswar@users.noreply.github.com>
2026-07-28 15:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (16.35%) - LeetSync

--- 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -3,9 +3,9 @@ public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         
         int n = arr.size();
-        
+       
         sort(arr.begin(), arr.end());
-        int diff = arr[1] - arr[0];
+         int diff = arr[1] - arr[0];
         // for(int i=0;i<n;i++){
         //     if(arr[i] < 0){
         //         arr[i] = -arr[i];
